857535b45859f97afe6e99dd33957f9f3a327eca
YTGachaForeverCorn <79651055+YTGachaForeverCorn@users.noreply.github.com>
2021-05-07 20:47:24 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+
+      
+Webcam.attach( '#camera' );
+
+camera = document.getElementById("camera");
+      
+  Webcam.set({
+    width:350,
+    height:300,
+    image_format : 'png',
+    png_quality:90
+  });
+
+function take_snapshot()
+{
+    Webcam.snap(function(data_uri){
+        document.getElementById("result").innerHTML = '<img id="selfie_image" src="'+data_uri+'"/>';
+    });
+}
